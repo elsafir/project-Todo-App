5eb7dfe15aa854a582f7d8d854fd7209c2e10099
el_safir <alfulsafiraaa08@gmail.com>
2024-06-10 13:29:50 +0700

    Develop toggle todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import Todos from './components/Todos';
 
-function App() {
+const App = () => {
   const [todos, setTodos] = useState([
     {
-      id: 1,
+      id:1,
       title: 'Finish Progate React Course',
       completed: false,
     },
@@ -20,15 +20,26 @@ function App() {
     },
   ])
 
-  console.log(todos)
+ const toggleCompleted = (todoId) => {
+  const updateTodos = todos.map((todo) => {
+    if (todo.id === todoId) {
+      todo.completed = !todo.completed
+    }
+    return todo
+  })
+  setTodos(updateTodos)
+ }
 
   return (
     <div style={styles.container}>
-      <h1 style={styles.title}>My Todo List</h1>
-      <Todos todos={todos} />
+      <h1 style={styles.title}>Todo List</h1>
+      <Todos todos={todos} 
+        toggleCompleted={toggleCompleted}
+      />
     </div>
-  )
+  );
 }
+
 const styles = {
   container: {
     textAlign: 'center',
@@ -38,4 +49,4 @@ const styles = {
     fontSize: '36px',
   },
 }
-export default App
+export default App;
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import TodoItem from './TodoItem'
-const Todos = ({ todos }) => {
-  return (
-    <div style={styles.container}>
-      {todos.map((todo) => {
-        return <TodoItem key={todo.id} todo={todo} />
-      })}
-    </div>
-  )
+
+const Todos = ({todos, toggleCompleted}) => {
+    return (
+        <div style={styles.container}>
+           {todos.map((todo) => {
+            return <TodoItem key={todo.id} todo={todo}
+            toggleCompleted={toggleCompleted}
+            />
+           })}
+        </div>
+    )
 }
 
 const styles = {
-  container: {
-    width: '40%',
-    margin: '0 auto',
-  },
+    container: {
+        width: '40%',
+        margin: '0 auto',
+    }
 }
 
 export default Todos
